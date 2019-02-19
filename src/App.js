1daf306fc94fa880783d7e833624abcd216261d4
Timur Asayonok <t.asaynok@gmail.com>
2019-02-19 11:22:01 +0100

    styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import {
-  Jumbotron, Button, Row, Col
-} from 'reactstrap';
+import { Row, Col, Container } from 'reactstrap';
 
-import logo from './logo.svg';
 import './App.css';
 
 const PictureComponent = (props) => {
@@ -29,21 +26,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Jumbotron className="main">
+        <Container>
           <h1 className="display-3">React app img WebP test!</h1>
           <Row className="d-flex justify-content-center mt-4">
             <Col xm="4">
-              <h5>
-                Normal JPEG image for all Browsers with img default type
-              </h5>
+              <Row className="col-header">
+                <h5>Normal JPEG image for all Browsers with img default type</h5>
+              </Row>
               <div>
                 <img src={require('./imges/1.sm.jpg')} />
               </div>
             </Col>
             <Col xm="4">
-              <h5>
-                WebP image. This image won't be shown at Safary and etc. (all list of browsers you can check here..) with img type `type="image/webp"`
-              </h5>
+              <Row className="col-header">
+                <h5>WebP image. This image won't be shown at Safary and etc. (all list of browsers you can check here..) with img type `type="image/webp"`</h5>
+              </Row>
               <div>
                 <img type="image/webp" src={require('./imges/1.sm.webp')} />
               </div>
@@ -55,16 +52,14 @@ class App extends Component {
                 {`Image container with the <picture> element, followed by two different <source> elements. In Safari browser will be shown JPEG image, in another will be chosen WebP image`}
               </h5>
               <PictureComponent
-                src={
-                  {
-                    webp: 'https://www.gstatic.com/webp/gallery/2.sm.webp',
-                    jpg: 'https://www.gstatic.com/webp/gallery/1.sm.jpg'
-                  }
-                }
+                src={{
+                  webp: 'https://www.gstatic.com/webp/gallery/2.sm.webp',
+                  jpg: 'https://www.gstatic.com/webp/gallery/1.sm.jpg'
+                }}
               />
             </Col>
           </Row>
-        </Jumbotron>
+        </Container>
       </div>
     );
   }
